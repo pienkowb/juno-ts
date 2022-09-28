7b74e73b4ce1e0ee377cd780f6b663e98a34d294
Bartosz Pieńkowski <pienkowb@gmail.com>
2022-09-28 19:13:07 +0200

    Format TypeScript files with Prettier

--- test/cosmwasm.spec.ts
+++ test/cosmwasm.spec.ts
@@ -1,15 +1,19 @@
 /// <reference types="@as-pect/assembly/types/as-pect" />
 
-import { MsgExecuteContract, encodeMsgExecuteContract, decodeMsgExecuteContract } from '../src/cosmwasm/wasm/v1/tx';
-import { Coin } from '../src/cosmos/base/v1beta1/coin';
+import {
+  MsgExecuteContract,
+  encodeMsgExecuteContract,
+  decodeMsgExecuteContract,
+} from "../src/cosmwasm/wasm/v1/tx";
+import { Coin } from "../src/cosmos/base/v1beta1/coin";
 
-describe('CosmWasm', () => {
-  test('decodes a contract execution message', () => {
+describe("CosmWasm", () => {
+  test("decodes a contract execution message", () => {
     const msg = new MsgExecuteContract(
-      'juno1hjct6q7npsspsg3dgvzk3sdf89spmlpf74d4wz',
-      'juno15u3dt79t6sxxa3x3kpkhzsy56edaa5a66wvt3kxmukqjz2sx0hes5sn38g',
+      "juno1hjct6q7npsspsg3dgvzk3sdf89spmlpf74d4wz",
+      "juno15u3dt79t6sxxa3x3kpkhzsy56edaa5a66wvt3kxmukqjz2sx0hes5sn38g",
       new Uint8Array(128),
-      [new Coin('juno', '200')]
+      [new Coin("juno", "200")]
     );
 
     const encodedMsg = encodeMsgExecuteContract(msg);
@@ -18,12 +22,12 @@ describe('CosmWasm', () => {
     expect(decodedMsg.sender).toBe(msg.sender);
     expect(decodedMsg.contract).toBe(msg.contract);
 
-    expect(decodedMsg.msg).toHaveLength(128)
+    expect(decodedMsg.msg).toHaveLength(128);
     expect(decodedMsg.funds).toHaveLength(1);
 
     const coin = decodedMsg.funds[0] as Coin;
 
-    expect(coin.denom).toBe('juno');
-    expect(coin.amount).toBe('200');
+    expect(coin.denom).toBe("juno");
+    expect(coin.amount).toBe("200");
   });
 });
